cb22c7831c94402bc3dbc337dc7d93fe3973a0d7
Sohini Das <d18123624@mydit.ie>
2019-04-19 22:04:14 +0000

    Generated production dist FE

--- blood_donor/credentials.py
+++ blood_donor/credentials.py
@@ -4,5 +4,5 @@ DB_DATABASE = "blood_donor_v1"
 DB_USERNAME = "blood_donor_db_user"
 DB_PASSWORD = "y63JwDf93fvuNA4"
 DJANGO_SECRET_KEY= "isi^y787kw9zw*-bk^0q03(i&78z#3(zmqvkn9lm$c5s@1fkw#"
-DJANGO_DEBUG_MODE=True
+DJANGO_DEBUG_MODE=False
 GOOGLE_MAPS_API_KEY="AIzaSyB2s22dOCpytjh2IT9Y7uCJOFw9NlXLIxM"
